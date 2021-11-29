33e36fdead14a1e948ad696aa7a95d4fb2086fc0
Sully12310 <flagsully@gmail.com>
2021-11-29 13:06:45 -0700

    Delete GameCFG.ts

--- website/Levels/modules/js/GameCFG.ts
+++
@@ -1,9 +0,0 @@
-import GameState from "./GameState";
-import GameWindow from "./GameWindow";
-
-export default interface GameCFG {
-    defaultGameState : GameState;
-    gameID : number;
-    window : GameWindow
-    checkWinCondition:() => boolean;
-}
